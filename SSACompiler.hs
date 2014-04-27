a7592056e69906fbd9712cc8c428def77211b056
Chris Wendt <chrismwendt@gmail.com>
2014-04-27 02:19:49 -0400

    Refactor SSA datatypes, parameterize extra information

--- SSACompiler.hs
+++ SSACompiler.hs
@@ -4,131 +4,137 @@ import qualified AST
 import Text.Printf
 import Data.List
 
-data SSAProgram = SSAProgram [SSAStatement] [SSAClass]
+data SSAProgram info = SSAProgram AST.Program [SSAStatement info] [SSAClass info]
 
-data SSAMethod = SSAMethod AST.MethodDecl [SSAStatement]
+data SSAClass info = SSAClass AST.ClassDecl [SSAField] [SSAMethod info]
 
-data SSAClass = SSAClass AST.ClassDecl [SSAField] [SSAMethod]
+data SSAMethod info = SSAMethod AST.MethodDecl [SSAParameter info] [SSAStatement info] (SSAReturn info)
 
-data SSAField = SSAField AST.VarDecl String Int StaticType
+data SSAField info = SSAField AST.VarDecl Int info
 
-data SSACall = SSACall String [SSAStatement]
+data SSAParameter info = SSAParameter AST.Parameter Int info
 
-data SSAStatement = SSAStatement { ssaID :: Int, ssaReg :: Maybe Int, ssaPinned :: Bool, ssaOp :: Op, ssaType :: Maybe StaticType }
+data SSAArgument info = SSAArgument (SSAStatement info) Int info
 
-data Op =
-      Unify SSAStatement SSAStatement
-    | Alias SSAStatement
+data SSAReturn info = SSAReturn (SSAStatement info)
+
+data SSALabel info = SSALabel String info
+
+data SSAStatement info =
+      Unify (SSAStatement info) (SSAStatement info)
+    | Alias (SSAStatement info)
     | This
-    | Parameter Int
-    | Arg SSAStatement Int
+    | Parameter (SSAParameter info)
+    | Arg (SSAArgument info)
     | Null StaticType
     | SInt Int
     | SBoolean Bool
     | NewObj String
-    | NewIntArray SSAStatement
-    | Label String
-    | Goto String
-    | Branch SSAStatement String
-    | NBranch SSAStatement String
-    | Call SSAStatement SSACall
-    | Print SSAStatement
-    | Return SSAStatement
-    | Member SSAStatement String
-    | Index SSAStatement SSAStatement
-    | Store SSAStatement Int
+    | NewIntArray (SSAStatement info)
+    | Label (SSALabel info)
+    | Goto (SSALabel info)
+    | Branch (SSAStatement info) (SSALabel info)
+    | NBranch (SSAStatement info) (SSALabel info)
+    | Call AST.MethodDecl (SSAStatement info) [SSAArgument info]
+    | Print (SSAStatement info)
+    | Return (SSAStatement info)
+    | Member (SSAStatement info) String
+    | Index (SSAStatement info) (SSAStatement info)
+    | Store (SSAStatement info) Int
     | Load Int
-    | VarAssg SSAStatement String
-    | MemberAssg SSAStatement SSAStatement String
-    | IndexAssg SSAStatement SSAStatement SSAStatement
-    | Not SSAStatement
-    | Lt SSAStatement SSAStatement
-    | Le SSAStatement SSAStatement
-    | Eq SSAStatement SSAStatement
-    | Ne SSAStatement SSAStatement
-    | Gt SSAStatement SSAStatement
-    | Ge SSAStatement SSAStatement
-    | And SSAStatement SSAStatement
-    | Or SSAStatement SSAStatement
-    | Plus SSAStatement SSAStatement
-    | Minus SSAStatement SSAStatement
-    | Mul SSAStatement SSAStatement
-    | Div SSAStatement SSAStatement
-    | Mod SSAStatement SSAStatement
+    | VarAssg (SSAStatement info) String
+    | MemberAssg (SSAStatement info) (SSAStatement info) String
+    | IndexAssg (SSAStatement info) (SSAStatement info) (SSAStatement info)
+    | Not (SSAStatement info)
+    | Lt (SSAStatement info) (SSAStatement info)
+    | Le (SSAStatement info) (SSAStatement info)
+    | Eq (SSAStatement info) (SSAStatement info)
+    | Ne (SSAStatement info) (SSAStatement info)
+    | Gt (SSAStatement info) (SSAStatement info)
+    | Ge (SSAStatement info) (SSAStatement info)
+    | And (SSAStatement info) (SSAStatement info)
+    | Or (SSAStatement info) (SSAStatement info)
+    | Plus (SSAStatement info) (SSAStatement info)
+    | Minus (SSAStatement info) (SSAStatement info)
+    | Mul (SSAStatement info) (SSAStatement info)
+    | Div (SSAStatement info) (SSAStatement info)
+    | Mod (SSAStatement info) (SSAStatement info)
 
 data StaticType =
       TypeInt
     | TypeBoolean
-    | TypeObject StaticObjectType
+    | TypeObject StaticTypeObject
 
-data StaticObjectType = StaticObjectType String (Maybe StaticObjectType)
+data StaticTypeObject = StaticTypeObject String (Maybe StaticTypeObject)
 
-instance Show SSAProgram where
-    show (SSAProgram m cs) = printf "program:\n%s%s" (show m) (concatMap show cs)
-
-instance Show SSAMethod where
-    show (SSAMethod (AST.MethodDecl _ name _ _ _ _) ss) = printf "    method %s:\n%s" name (concatMap show ss)
-
-instance Show SSAClass where
-    show (SSAClass (AST.ClassDecl name _ _ _) fs ms) = printf "  class %s:\n%s" name (concatMap show ms)
-
-instance Show SSAField where
-    show (SSAField vd name index t) = name
-
-instance Show SSACall where
-    show (SSACall name ss) = printf "    method %s:\n%s" name (concatMap show ss)
-
-instance Show SSAStatement where
-    show (SSAStatement index (Just reg) pin s (Just t)) = printf "      %d(%d): %s :%s\n" index reg (show s) (show t)
-    show (SSAStatement index Nothing pin s (Just t)) = printf "      %d: %s :%s\n" index (show s) (show t)
-    show (SSAStatement index (Just reg) pin s Nothing) = printf "      %d(%d): %s\n" index reg (show s)
-    show (SSAStatement index Nothing pin s Nothing) = printf "      %d: %s\n" index (show s)
-
-instance Show Op where
-    show (Unify (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Unify %d %d" i1 i2
-    show (Alias (SSAStatement { ssaID = i1 })) = printf "Alias %d" i1
-    show This = printf "This"
-    show (Parameter i) = printf "Parameter *%d" i
-    show (Arg (SSAStatement { ssaID = i1 }) i2) = printf "Arg %d *%d" i1 i2
-    show (Null t) = printf "Null *Type(%s)" (show t)
-    show (SInt v) = printf "Int *%d" v
-    show (SBoolean v) = printf "Boolean *%s" (if v then "true" else "false")
-    show (NewObj name) = printf "NewObj *%s" name
-    show (NewIntArray (SSAStatement { ssaID = i })) = printf "NewIntArray *%d" i
-    show (Label label) = printf "Label *%s" label
-    show (Branch (SSAStatement { ssaID = i }) label) = printf "Branch %d *%s" i label
-    show (NBranch (SSAStatement { ssaID = i }) label) = printf "NBranch %d *%s" i label
-    show (Call (SSAStatement { ssaID = i }) (SSACall name args)) = printf "Call %d *%s(%s)" i name (intercalate ", " $ map (show . ssaID) args)
-    show (Print (SSAStatement { ssaID = i })) = printf "Print %d" i
-    show (Return (SSAStatement { ssaID = i })) = printf "Return %d" i
-    show (Member (SSAStatement { ssaID = i }) name) = printf "Member %d *%s" i name
-    show (Index (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Index %d %s" i1 i2
-    show (Store (SSAStatement { ssaID = i }) index) = printf "Store %d *%d" i index
-    show (Load index) = printf "Load *%d" index
-    show (VarAssg (SSAStatement { ssaID = i }) name) = printf "VarAssg %d *%s" i name
-    show (MemberAssg (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 }) name) = printf "MemberAssg %d %d *%s" i1 i2 name
-    show (IndexAssg (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 }) s) = printf "IndexAssg %d %d *%s" i1 i2 (show s)
-    show (Not (SSAStatement { ssaID = i1 })) = printf "Not %d" i1
-    show (Lt (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Lt %d %d" i1 i2
-    show (Le (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Le %d %d" i1 i2
-    show (Eq (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Eq %d %d" i1 i2
-    show (Ne (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Ne %d %d" i1 i2
-    show (Gt (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Gt %d %d" i1 i2
-    show (Ge (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Ge %d %d" i1 i2
-    show (And (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "And %d %d" i1 i2
-    show (Or (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Or %d %d" i1 i2
-    show (Plus (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Plus %d %d" i1 i2
-    show (Minus (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Minus %d %d" i1 i2
-    show (Mul (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Mul %d %d" i1 i2
-    show (Div (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Div %d %d" i1 i2
-    show (Mod (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Mod %d %d" i1 i2
-
-instance Show StaticType where
-    show TypeInt = "Type(int)"
-    show TypeBoolean = "Type(boolean)"
-    show (TypeObject t) = show t
-
-instance Show StaticObjectType where
-    show (StaticObjectType name _) = printf "Type(%s)" name
-
-ssaCompileProgram ast@(AST.Program s cs) = SSAProgram (ssaCompileStatement ast s) undefined
+-- instance Show SSAProgram where
+--     show (SSAProgram m cs) = printf "program:\n%s%s" (show m) (concatMap show cs)
+--
+-- instance Show SSAMethod where
+--     show (SSAMethod (AST.MethodDecl _ name _ _ _ _) ss) = printf "    method %s:\n%s" name (concatMap show ss)
+--
+-- instance Show SSAClass where
+--     show (SSAClass (AST.ClassDecl name _ _ _) fs ms) = printf "  class %s:\n%s" name (concatMap show ms)
+--
+-- instance Show SSAField where
+--     show (SSAField vd name index t) = name
+--
+-- instance Show SSACall where
+--     show (SSACall name ss) = printf "    method %s:\n%s" name (concatMap show ss)
+--
+-- instance Show SSAStatement where
+--     show (SSAStatement index (Just reg) pin s (Just t)) = printf "      %d(%d): %s :%s\n" index reg (show s) (show t)
+--     show (SSAStatement index Nothing pin s (Just t)) = printf "      %d: %s :%s\n" index (show s) (show t)
+--     show (SSAStatement index (Just reg) pin s Nothing) = printf "      %d(%d): %s\n" index reg (show s)
+--     show (SSAStatement index Nothing pin s Nothing) = printf "      %d: %s\n" index (show s)
+--
+-- instance Show Op where
+--     show (Unify (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Unify %d %d" i1 i2
+--     show (Alias (SSAStatement { ssaID = i1 })) = printf "Alias %d" i1
+--     show This = printf "This"
+--     show (Parameter i) = printf "Parameter *%d" i
+--     show (Arg (SSAStatement { ssaID = i1 }) i2) = printf "Arg %d *%d" i1 i2
+--     show (Null t) = printf "Null *Type(%s)" (show t)
+--     show (SInt v) = printf "Int *%d" v
+--     show (SBoolean v) = printf "Boolean *%s" (if v then "true" else "false")
+--     show (NewObj name) = printf "NewObj *%s" name
+--     show (NewIntArray (SSAStatement { ssaID = i })) = printf "NewIntArray *%d" i
+--     show (Label label) = printf "Label *%s" label
+--     show (Branch (SSAStatement { ssaID = i }) label) = printf "Branch %d *%s" i label
+--     show (NBranch (SSAStatement { ssaID = i }) label) = printf "NBranch %d *%s" i label
+--     show (Call (SSAStatement { ssaID = i }) (SSACall name args)) = printf "Call %d *%s(%s)" i name (intercalate ", " $ map (show . ssaID) args)
+--     show (Print (SSAStatement { ssaID = i })) = printf "Print %d" i
+--     show (Return (SSAStatement { ssaID = i })) = printf "Return %d" i
+--     show (Member (SSAStatement { ssaID = i }) name) = printf "Member %d *%s" i name
+--     show (Index (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Index %d %s" i1 i2
+--     show (Store (SSAStatement { ssaID = i }) index) = printf "Store %d *%d" i index
+--     show (Load index) = printf "Load *%d" index
+--     show (VarAssg (SSAStatement { ssaID = i }) name) = printf "VarAssg %d *%s" i name
+--     show (MemberAssg (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 }) name) = printf "MemberAssg %d %d *%s" i1 i2 name
+--     show (IndexAssg (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 }) s) = printf "IndexAssg %d %d *%s" i1 i2 (show s)
+--     show (Not (SSAStatement { ssaID = i1 })) = printf "Not %d" i1
+--     show (Lt (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Lt %d %d" i1 i2
+--     show (Le (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Le %d %d" i1 i2
+--     show (Eq (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Eq %d %d" i1 i2
+--     show (Ne (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Ne %d %d" i1 i2
+--     show (Gt (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Gt %d %d" i1 i2
+--     show (Ge (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Ge %d %d" i1 i2
+--     show (And (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "And %d %d" i1 i2
+--     show (Or (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Or %d %d" i1 i2
+--     show (Plus (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Plus %d %d" i1 i2
+--     show (Minus (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Minus %d %d" i1 i2
+--     show (Mul (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Mul %d %d" i1 i2
+--     show (Div (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Div %d %d" i1 i2
+--     show (Mod (SSAStatement { ssaID = i1 }) (SSAStatement { ssaID = i2 })) = printf "Mod %d %d" i1 i2
+--
+-- instance Show StaticType where
+--     show TypeInt = "Type(int)"
+--     show TypeBoolean = "Type(boolean)"
+--     show (TypeObject t) = show t
+--
+-- instance Show StaticTypeObject where
+--     show (StaticTypeObject name _) = printf "Type(%s)" name
+--
+-- ssaCompileProgram ast@(AST.Program s cs) = SSAProgram (ssaCompileStatement ast s) (map (ssaCompileClassDecl ast) cs)
+-- ssaCompileStatement ast (AST.BlockStatement ss) = undefined --concatMap (ssaCompileStatement ast) ss
+-- ssaCompileClassDecl ast (AST.ClassDecl name _ _ ms) = undefined --SSAClass
