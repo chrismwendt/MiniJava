0b0ea1a2201c732fb5f77b535c30f7b992493887
Chris Wendt <chrismwendt@gmail.com>
2014-04-27 23:51:40 -0400

    Unqualify a Parsec import

--- Lexer.hs
+++ Lexer.hs
@@ -3,14 +3,14 @@ module Lexer where
 import qualified Text.ParserCombinators.Parsec.Token as Token
 import Text.ParserCombinators.Parsec.Language
 import Text.ParserCombinators.Parsec
-import qualified Text.Parsec.Prim as P
+import Text.Parsec.Prim
 
 languageDef = emptyDef
     { Token.commentStart    = "/*"
     , Token.commentEnd      = "*/"
     , Token.commentLine     = "//"
     , Token.identStart      = letter
-    , Token.identLetter     = alphaNum P.<|> char '_'
+    , Token.identLetter     = alphaNum <|> char '_'
     , Token.reservedNames   = words "class main public static void extends return int boolean if else while System.out.println true false this new"
     }
 
