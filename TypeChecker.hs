5eae0710e95140d94c6bec674afd1aa3b007651d
Chris Wendt <chrismwendt@gmail.com>
2014-04-28 03:57:57 -0400

    Build class hierarchy

--- TypeChecker.hs
+++ TypeChecker.hs
@@ -5,6 +5,8 @@ import SSACompiler
 import qualified Data.Map as M
 import Control.Monad.State
 import Safe
+import Data.Maybe
+import Data.Graph.Inductive (mapFst, mapSnd)
 
 type TypeInfo = (Int, StaticType)
 
@@ -28,8 +30,23 @@ builtInTypes = let object = StaticTypeObject "Object" Nothing
         , ("Object", TypeObject object)
         ]
 
+extract :: (a -> Maybe b) -> [a] -> (Maybe (a, b), [a])
+extract f [] = (Nothing, [])
+extract f (a:as) = case f a of
+    Just b -> (Just (a, b), as)
+    Nothing -> let (maybeB, rest) = extract f as in (maybeB, a:rest)
+
 augment :: M.Map String StaticType -> AST.Program -> M.Map String StaticType
-augment = undefined
+augment types (AST.Program _ classDecls) = moreTypes
+    where
+    pair (ClassDecl name super _ _) = (name, super)
+    orphans = map (mapSnd (fromJustDef "Object") . pair) classDecls
+    insertOrphan ([], m) = ([], m)
+    insertOrphan (os, m) = case extract (flip M.lookup m . snd) os of
+        (Nothing, _) -> error $ "Orphan classes: " ++ unwords (map fst os)
+        (Just ((childName, _), parentType@(TypeObject t)), os') -> (os', M.insert childName (TypeObject (StaticTypeObject childName (Just t))) m)
+        (Just (_, parentType), _) -> error $ "No class can extend " ++ show parentType
+    moreTypes = snd $ until (null . fst) insertOrphan (orphans, types)
 
 typeCheck :: SSAProgram Int -> SSAProgram TypeInfo
 typeCheck program@(SSAProgram ast _ _) = evalState (tcProgram program) TypeState
