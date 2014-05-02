282e8ff0b7b30c3d0c2e13ba6a5b145439f2bb93
Chris Wendt <chrismwendt@gmail.com>
2014-05-01 23:20:48 -0400

    WIP on register allocator

--- RegisterAllocator.hs
+++ RegisterAllocator.hs
@@ -1,12 +1,69 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module RegisterAllocator where
 
 import qualified AST as AST
 import SSACompiler
 import qualified Data.Map as M
+import qualified Data.Set as S
 import Control.Monad.State
 import Data.List
+import Control.Lens
 
 type Register = Int
 
-allocate :: SSAProgram ID StaticType -> [ID] -> M.Map ID (SSAStatement ID StaticType) -> (SSAProgram ID (StaticType, Register), [ID], M.Map ID (SSAStatement ID (StaticType, Register)))
-allocate program m = undefined
+type Variable = S.Set (SSAStatement ID StaticType)
+
+data CFNode = CFNode
+    { _defs  :: S.Set Variable
+    , _uses  :: S.Set Variable
+    , _preds :: S.Set Variable
+    , _succs :: S.Set Variable
+    , _ins   :: S.Set Variable
+    , _outs  :: S.Set Variable
+    }
+    deriving (Show)
+
+data TempNode = TempNode
+    { _color :: Register
+    , _variable :: Variable
+    , _neighbors :: S.Set TempNode
+    }
+
+data RegState = RegState
+    { _sToV :: M.Map ID Variable
+    , _vToT :: M.Map Variable TempNode
+    , _nonSpills :: S.Set TempNode
+    , _potentialSpills :: S.Set TempNode
+    , _vToSSAs :: M.Map Variable (S.Set ID)
+    , _spillCount :: Int
+    , _idList :: [ID]
+    , _idToS :: M.Map ID (SSAStatement ID StaticType)
+    , _idToS' :: M.Map ID (SSAStatement ID (StaticType, Register))
+    }
+
+makeLenses ''CFNode
+makeLenses ''TempNode
+makeLenses ''RegState
+
+allocate ::
+       SSAProgram ID StaticType
+    -> [ID]
+    -> M.Map ID (SSAStatement ID StaticType)
+    -> (SSAProgram ID (StaticType, Register), [ID], M.Map ID (SSAStatement ID (StaticType, Register)))
+allocate program ss m = (a, s ^. idList, s ^. idToS')
+    where
+    (a, s) = runState (allocProgram program) RegState
+        { _sToV = M.empty
+        , _vToT = M.empty
+        , _nonSpills = S.empty
+        , _potentialSpills = S.empty
+        , _vToSSAs = M.empty
+        , _spillCount = 0
+        , _idList = ss
+        , _idToS = m
+        , _idToS' = M.empty
+        }
+
+allocProgram :: SSAProgram ID StaticType -> State RegState (SSAProgram ID (StaticType, Register))
+allocProgram program = undefined
