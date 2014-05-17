68addef6996dea3e5dd23fee4b2dccc9a118910e
Chris Wendt <chrismwendt@gmail.com>
2014-05-16 21:46:17 -0400

    Whitespace

--- RegisterAllocator.hs
+++ RegisterAllocator.hs
@@ -69,17 +69,20 @@ spillReg sc r g = flip execState g $ do
     mapM_ (modifyGraph storeReg) $ filterBy (maybeToSet . _lDef)
     where
     filterBy f = filter (\n -> r `Set.member` (f $ fromJust $ G.lab g n)) (G.nodes g)
+
     modifyGraph f n = do
         g <- get
         case G.match n g of
             (Nothing, _) -> error "match failure"
             a@(Just (ins, _, LiveLabel stu ds us vIns vOuts, outs), g') -> f a
+
     loadReg (Just (ins, n, LiveLabel stu ds us vIns vOuts, outs), g') = do
         g <- get
         let allRegs = [def | (_, LiveLabel { _lDef = Just def }) <- G.labNodes g]
             avail = head $ Set.toList $ Set.fromList allRegs `Set.difference` (Set.delete r vIns)
             load = (ins, head (G.newNodes 1 g), LiveLabel (R.Load sc avail) (Just avail) us vIns vOuts, [])
         put (([(S.Step, G.node' load)], n, LiveLabel (R.mapRegs (\x -> if x == r then avail else x) stu) ds us vIns vOuts, outs) G.& (load G.& g'))
+
     storeReg (Just (ins, n, label@(LiveLabel stu ds us vIns vOuts), outs), g') = do
         g <- get
         let store = ([(S.Step, n)], head (G.newNodes 1 g), LiveLabel (R.Store r sc) ds us vIns vOuts,  outs)
